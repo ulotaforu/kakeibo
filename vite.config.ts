465a99c29595c56e2a12dd1a40eb1e75996bff8c
ulotaforu <ulotaforu@gmail.com>
2025-04-23 14:14:19 +0000

    hono+react routerのテンプレを追加

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+// vite.config.ts
+import adapter from '@hono/vite-dev-server/cloudflare'
+import { reactRouter } from '@react-router/dev/vite'
+import { cloudflareDevProxy as remixCloudflareDevProxy } from '@react-router/dev/vite/cloudflare'
+import serverAdapter from 'hono-react-router-adapter/vite'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { getLoadContext } from './load-context'
+
+export default defineConfig({
+  server: {
+    port: 3000,
+    host: "0.0.0.0"
+  },
+  plugins: [
+    remixCloudflareDevProxy(),
+    reactRouter(),
+    serverAdapter({
+      adapter,
+      getLoadContext,
+      entry: 'server/index.ts',
+    }),
+    tsconfigPaths(),
+  ],
+})
