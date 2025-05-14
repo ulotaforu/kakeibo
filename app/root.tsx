5316708eb39a9681a67cd52641fc73336c2bcaab
ulotaforu <ulotaforu@gmail.com>
2025-05-14 14:31:54 +0000

    bodyのmarginを0にする

--- app/root.tsx
+++ app/root.tsx
@@ -1,20 +1,22 @@
-import { Outlet, Scripts } from 'react-router'
+import { Outlet, Scripts } from "react-router";
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang='en'>
-      <head>
-        <meta charSet='utf-8' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-      </head>
-      <body>
-        {children}
-        <Scripts />
-      </body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</head>
+			<body style={{ margin: 0 }}>
+				<Theme>{children}</Theme>
+				<Scripts />
+			</body>
+		</html>
+	);
 }
 
 export default function App() {
-  return <Outlet />
+	return <Outlet />;
 }
