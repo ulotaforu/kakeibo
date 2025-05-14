fda56b4e87d42fab34bdb2d4500dac0ff2a36832
ulotaforu <ulotaforu@gmail.com>
2025-05-14 14:32:46 +0000

    トップページを実装

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,29 +1,43 @@
-import SignOutButton from '~/components/SignOutButton'
-import type { Route } from './+types/_index'
+import type { Route } from "./+types/_index";
+import { Button, Container, Flex, Heading } from "@radix-ui/themes";
+import { signIn } from "@hono/auth-js/react";
 
 export const loader = (args: Route.LoaderArgs) => {
-  const extra = args.context.extra
-  const cloudflare = args.context.cloudflare
-  const myVarInVariables = args.context.hono.context.get('MY_VAR_IN_VARIABLES')
-  const isWaitUntilDefined = !!cloudflare.ctx.waitUntil
-  return { cloudflare, extra, myVarInVariables, isWaitUntilDefined }
-}
+	const extra = args.context.extra;
+	const cloudflare = args.context.cloudflare;
+	const myVarInVariables = args.context.hono.context.get("MY_VAR_IN_VARIABLES");
+	const isWaitUntilDefined = !!cloudflare.ctx.waitUntil;
+	return { cloudflare, extra, myVarInVariables, isWaitUntilDefined };
+};
 
 export default function Index({ loaderData }: Route.ComponentProps) {
-  const { cloudflare, extra, myVarInVariables, isWaitUntilDefined } = loaderData
-  return (
-    <div>
-      <h1>React Router and Hono</h1>
-      <h2>Var is {cloudflare.env.MY_VAR}</h2>
-      <h3>
-        {cloudflare.cf ? 'cf,' : ''}
-        {cloudflare.ctx ? 'ctx,' : ''}
-        {cloudflare.caches ? 'caches are available' : ''}
-      </h3>
-      <h4>Extra is {extra}</h4>
-      <h5>Var in Variables is {myVarInVariables}</h5>
-      <h6>waitUntil is {isWaitUntilDefined ? 'defined' : 'not defined'}</h6>
-      <SignOutButton/>
-    </div>
-  )
+	const { cloudflare, extra, myVarInVariables, isWaitUntilDefined } =
+		loaderData;
+	return (
+		<Container size={"3"} pt={"4"}>
+			<Heading size={"8"} align="center">
+				Kakeibo
+			</Heading>
+			<Flex align={"center"} justify={"center"} pt={"4"}>
+				<Button
+					variant="surface"
+					onClick={() => signIn("google", { callbackUrl: "/home" })}
+				>
+					Googleアカウントでサインイン
+				</Button>
+			</Flex>
+		</Container>
+		// <div>
+		//   <h1>Kakeibo</h1>
+		//   <h2>Var is {cloudflare.env.MY_VAR}</h2>
+		//   <h3>
+		//     {cloudflare.cf ? 'cf,' : ''}
+		//     {cloudflare.ctx ? 'ctx,' : ''}
+		//     {cloudflare.caches ? 'caches are available' : ''}
+		//   </h3>
+		//   <h4>Extra is {extra}</h4>
+		//   <h5>Var in Variables is {myVarInVariables}</h5>
+		//   <h6>waitUntil is {isWaitUntilDefined ? 'defined' : 'not defined'}</h6>
+		// </div>
+	);
 }
